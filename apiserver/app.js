49ef5c1f3a6bd6c150ee31ea38003384e1a956b3
Liwenxiu <742952713@qq.com>
2019-11-21 20:41:39 +0800

    重新提交服务器代码

--- apiserver/app.js
+++ apiserver/app.js
@@ -1,28 +1,73 @@
 // 导入express
-const express = require('express');
+const express = require('express')
 // 日志中间件
-const morgan = require('morgan');
+const morgan = require('morgan')
 // cors中间件
-const cors = require('cors');
+const cors = require('cors')
 // 导入路由
-const frontRouter = require('./router/frountRouter');
-const adminRouter = require('./router/adminRouter');
+
+const jwt = require('jsonwebtoken')
+const frontRouter = require('./router/frountRouter')
+const adminRouter = require('./router/adminRouter')
 // 实例化服务器对象
-const app = express();
+const app = express()
+// 导入bodyParser中间件
+const bodyParser = require('body-parser')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+var cookieParser = require('cookie-parser')
+app.use(cookieParser())
 // 日志中间件
-app.use(morgan('tiny'));
+app.use(morgan('tiny'))
 // cors
-app.use(cors());
+// app.use(cors());
+app.all('*', function (req, res, next) {
+  console.log(`${Date.now()}:来自${req.connection.remoteAddress} 访问了 ${req.method}-${req.url}。参数是：${req.query},携带cookie:${req.headers.cookie}`)
+  console.log('携带cookie:')
+  console.log(req.cookies)
+  res.header('Access-Control-Allow-Origin', req.headers.origin) // 需要显示设置来源
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Credentials', true) // 带cookies
+  next()
+})
 
 // 托管静态资源
-app.use('/static', express.static('uploads'));
+app.use('/static', express.static('uploads'))
+// 是否使用 权限
+console.log(process.argv)
 
+if (process.argv[2] === 'token') {
+  app.use('/admin', function (req, res, next) {
+    console.log(Date.now(), req.url)
+    if (req.url === '/login') {
+      next()
+    } else if (req.url === '/logout') {
+      // res.clearCookie('token');
+      res.cookie('token', '1', { expires: new Date(Date.now() - 1000 * 5) })
+      next()
+    } else {
+      const { token } = req.cookies
+      const secretOrPrivateKey = 'jwt' // 这是加密的key（密钥）
+      jwt.verify(token, secretOrPrivateKey, (err, decode) => {
+        console.log('jwt.verify', token)
+        if (err) {
+          //  时间失效的时候 || 伪造的token
+          res.statusCode = 403
+          res.send({ success: false, msg: '无权访问' })
+        } else {
+          next()
+        }
+      })
+    }
+  })
+}
 // 注册前台路由
-app.use('/', frontRouter);
+app.use('/', frontRouter)
 // 注册后台路由
-app.use('/admin', adminRouter);
+app.use('/admin', adminRouter)
 
 // 开启监听
 app.listen(8000, () => {
-  console.log('app已启动,端口为8000');
-});
+  console.log('app已启动,端口为8000')
+})
--- apiserver/router/frountRouter.js
+++ apiserver/router/frountRouter.js
@@ -1,33 +1,71 @@
 // 导入express
-const express = require('express')
+const express = require('express');
 // 导入路由
-const router = express.Router()
+const router = express.Router();
 // 导入信息
-const message = require('../utils/message')
+const message = require('../utils/message');
 // 导入数据
-const db = require('../utils/db')
+const db = require('../utils/db');
 
 // 文章搜索
-router.get('/search',(req,res)=>{
+router.get('/search', (req, res) => {
   // 参数获取
-  const key = req.query.key||''
-  const type = req.query.type||''
-  const page = req.query.page ||1
-  const perpage = req.query.perpage||6
+  const key = req.query.key || '';
+  const type = req.query.type || '';
+  const page = req.query.page || 1;
+  const perpage = req.query.perpage || 6;
 
   // 类型判断
-  if(isNaN(page)||isNaN(perpage)){
-    message.invalidParameter(res)
+  if (isNaN(page) || isNaN(perpage)) {
+    message.invalidParameter(res);
     return;
   }
   // 数据获取
-  const article = db.getArticle()
+  const article = db.getArticle();
 
   // 根据类型检索数据
-  
-  
+});
 
-})
+// 评论
+router.post('/post_comment', (req, res) => {
+  // 参数获取
+  const article_id = req.body.article_id;
+  const name = req.body.name;
+  const content = req.body.content;
+
+  const result = db.addComments({ name, content, article_id });
+  if (result) {
+    res.send({
+      msg: '添加评论成功,等待管理员批准',
+      code: 200
+    });
+  } else {
+    res.send({
+      msg: '添加评论不成功',
+      code: 400
+    });
+  }
+});
+
+// 获取指定文章编号的评论
+router.get('/get_comments', (req, res) => {
+  // 参数获取
+  const article_id = req.query.article_id;
+
+  const result = db.getComments({ article_id });
+  if (result) {
+    res.send({
+      msg: 'ok',
+      code: 200,
+      data: result
+    });
+  } else {
+    res.send({
+      msg: '获取评论不成功',
+      code: 400
+    });
+  }
+});
 
 // 暴露
-module.exports = router
+module.exports = router;
