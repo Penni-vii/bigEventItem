cb874c10e1db6b6d5090d571ece0cf399eb31687
Liwenxiu <742952713@qq.com>
2019-11-24 12:03:05 +0800

    筛选功能完成

--- web_back/js/utils/article.js
+++ web_back/js/utils/article.js
@@ -1,6 +1,6 @@
 var article = {
-  show: function (callback) {
-    $.get(APIURLS.article_show,function (res) {
+  show: function (curPage,type,state,callback) {
+    $.get(APIURLS.article_show,{page:curPage,type:type,state:state},function (res) {
       callback(res);
     });
   },
