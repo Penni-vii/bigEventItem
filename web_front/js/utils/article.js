32bc74c94f1b40057853cbef8677f6008034bf7c
Liwenxiu <742952713@qq.com>
2019-11-27 01:05:11 +0800

    客户端-查看文章详情完成

--- web_front/js/utils/article.js
+++ web_front/js/utils/article.js
@@ -1,7 +1,18 @@
 var article = {
-  show: function (curPage,type,state,callback) {
-    $.get(APIURLS.article_show,{page:curPage,type:type,state:state},function (res) {
+  getDetail: function (id,callback) {
+    $.get(APIURLS.article_show,{id:id},
+      function (res) {
       callback(res);
-    });
+    })
+  },
+  getFiveFocus: function (callback) {
+    $.get(APIURLS.article_show,
+      {
+        perpage: 5, // 总显示的文章数
+        state: '已发布'   // 文章状态
+      },
+      function (res) {
+      callback(res);
+    })
   }
 }
