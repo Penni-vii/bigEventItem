72e19a382e18bde0679380ebd7b7b3728c82a798
Liwenxiu <742952713@qq.com>
2019-11-26 17:34:53 +0800

    初始化客户端页面

---
+++ web_front/js/utils/config.js
@@ -0,0 +1,12 @@
+// 整个项目的基地址
+var baseUrl = 'http://localhost:8000/';
+
+
+// 整个大事件项目的接口地址
+var APIURLS = {
+
+  category_show: baseUrl + 'admin/category_search',
+
+  article_show: baseUrl + 'admin/search',
+};
+
