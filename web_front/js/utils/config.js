9cf6f4f2985a1769c3e88913dde3c0e1faf6da03
Liwenxiu <742952713@qq.com>
2019-12-03 11:35:05 +0800

    大事件项目完工

--- web_front/js/utils/config.js
+++ web_front/js/utils/config.js
@@ -8,5 +8,8 @@ var APIURLS = {
   category_show: baseUrl + 'admin/category_search',
 
   article_show: baseUrl + 'admin/search',
+
+  comment_add: baseUrl + 'post_comment',
+  comment_get: baseUrl + 'get_comments'
 };
 
